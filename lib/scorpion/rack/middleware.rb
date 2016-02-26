2fa068bd2c672cb4404035bbb7e328e64a4309ce
Paul Alexander <paul@phallguy.com>
2016-02-25 20:31:58 -0800

    Expose Rack ENV through scorpion

---
+++ lib/scorpion/rack/middleware.rb
@@ -0,0 +1,34 @@
+require "scorpion/rack/env"
+
+module Scorpion
+  module Rack
+    class Middleware
+
+      ENV_KEY = 'scorpion.rack.instance'.freeze
+
+      def initialize( app, nest = nil )
+        @app  = app
+        @nest = nest || Scorpion.instance.build_nest
+      end
+
+      def call( env )
+        env[ENV_KEY] = prepare_scorpion( nest.conceive, env )
+        @app.call(env).tap do
+          free_scorpion( env )
+        end
+      end
+
+      private
+        attr_reader :nest
+
+        def prepare_scorpion( scorpion, env )
+          scorpion.hunt_for Rack::Env, return: env
+          scorpion
+        end
+
+        def free_scorpion( env )
+          env[ENV_KEY].destroy
+        end
+    end
+  end
+end
---
+++ spec/lib/scorpion/rack/middleware_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+require "scorpion/rack/middleware"
+
+describe Scorpion::Rack::Middleware do
+  let(:app)      { proc{ [ 200, {}, [ 'Sting!' ] ] } }
+  let(:stack)    { Scorpion::Rack::Middleware.new( app ) }
+  let(:request)  { Rack::MockRequest.new( stack ) }
+  let(:response) { request.get('/') }
+
+  it "creates a scorpion" do
+    expect( stack ).to receive( :prepare_scorpion ).and_call_original
+    response
+  end
+
+  it "prepares it with the environment" do
+    proc = ->(env) {
+        scorpion = env[Scorpion::Rack::Middleware::ENV_KEY]
+        expect( scorpion.fetch( Scorpion::Rack::Env) ).to be env
+      app.call( env )
+    }
+
+    request  = Rack::MockRequest::new( Scorpion::Rack::Middleware.new( proc ) )
+    request.get '/'
+  end
+
+  it "frees the scorpion on return" do
+    expect( stack ).to receive( :free_scorpion ).and_call_original
+    response
+  end
+end
