fde9d2b97fbb545345e1bc0cf32dd81676401977
Paul Alexander <paul@bark.us>
2019-10-20 11:01:32 -0700

    bump to 1.0.7 to fix build problems

--- lib/scorpion/version.rb
+++ lib/scorpion/version.rb
@@ -1,5 +1,5 @@
 module Scorpion
-  VERSION_NUMBER  = "1.0.6".freeze
+  VERSION_NUMBER  = "1.0.7".freeze
   VERSION_SUFFIX  = "".freeze
   VERSION         = "#{ VERSION_NUMBER }#{ VERSION_SUFFIX }".freeze
 end
